f6cbec4ebefd55a885a913484d2b294a04fd3a4b
Stardidi <636535+Stardidi@users.noreply.github.com>
2019-02-10 20:35:28 +0100

    Added productivity popup timings
    - Renamed members according to naming conventions
    - Separated some functionality into functions
    - Productivity window is now non-blocking

--- ProductivityTracker.py
+++ ProductivityTracker.py
@@ -71,18 +71,24 @@ class AppServerSvc (win32serviceutil.ServiceFramework):
         self.main()
 
 
-    def on_mouse_move(self, x, y):
-        self.mouse_moves += 1
+    def OnMouseMove(self, x, y):
+        self.mMouseMoves += 1
         
-    def on_mouse_click(self, x, y, button, pressed):
+    def OnMouseClick(self, x, y, button, pressed):
         if pressed:
-            self.mouse_clicks += 1
+            self.mMouseClicks += 1
     
-    def on_mouse_scroll(self, x, y, dx, dy):
-        self.mouse_scrolls += 1
+    def OnMouseScroll(self, x, y, dx, dy):
+        self.mMouseScrolls += 1
 
-    def on_keyboard_press(self, key):
-        self.keyboard_presses += 1
+    def OnKeyboardPress(self, key):
+        self.mKeyboardPresses += 1
+
+    def OnLock(self):
+        print("Locked")
+    
+    def OnUnlock(self):
+        print("Unlocked")
 
 
     def start(self):               
@@ -97,93 +103,32 @@ class AppServerSvc (win32serviceutil.ServiceFramework):
             flow = client.flow_from_clientsecrets('credentials.json', SCOPES)
             creds = tools.run_flow(flow, store)
         service = build('sheets', 'v4', http=creds.authorize(Http()))
-        self.spreadsheets = service.spreadsheets()
+        self.mSpreadsheetsService = service.spreadsheets()
 
         # Initialize values
-        self.batches = {}
-        self.last_batch_send = 0
-        self.last_popup = time.time()
-        self.last_update = time.time()
-        self.mouse_moves = 0
-        self.mouse_clicks = 0
-        self.mouse_scrolls = 0
-        self.keyboard_presses = 0
-        self.icon = None
+        self.mBatches               = {}
+        self.mLastBatchSend         = -1
+        self.mLastPopup             = -1
+        self.mScheduledPopup        = -1
+        self.mLastUpdate            = time.time()
+        self.mLastInput             = time.time()
+        self.mLockedSince           = -1
+        self.mMouseMoves            = 0
+        self.mMouseClicks           = 0
+        self.mMouseScrolls          = 0
+        self.mKeyboardPresses       = 0
+        self.mTrayIcon              = None
+        self.mProductivityWindow    = None
 
         # Register mouse and keyboard listeners
-        self.mouse_listener = mouse.Listener(on_move=self.on_mouse_move, on_click=self.on_mouse_click, on_scroll=self.on_mouse_scroll)    # Should we exclude mouse moves? 
-        self.mouse_listener.start()
-        self.keyboard_listener = keyboard.Listener(on_press=self.on_keyboard_press)
-        self.keyboard_listener.start()
-        self.mouse_listener.wait()
-        self.keyboard_listener.wait()
+        self.mMouseListener = mouse.Listener(on_move=self.OnMouseMove, on_click=self.OnMouseClick, on_scroll=self.OnMouseScroll)    # Should we exclude mouse moves? 
+        self.mMouseListener.start()
+        self.mKeyboardListener = keyboard.Listener(on_press=self.OnKeyboardPress)
+        self.mKeyboardListener.start()
+        self.mMouseListener.wait()
+        self.mKeyboardListener.wait()
 
-        self.isrunning = True
-
-
-    def stop(self):
-        self.isrunning = False
-        self.mouse_listener.stop()
-
-    def QueueRow(self, inTable, inRowValues):
-        # Create new batch array if it does not exist yet
-        if inTable not in self.batches:
-            self.batches[inTable] = []
-
-        # Append this row to the batch
-        self.batches[inTable].append(inRowValues)
-
-    def UpdateBatches(self):
-        # Wait for the interval to elapse
-        if time.time() - self.last_batch_send < cSendInterval:
-            return
-        
-        # Send every batch in the dict of batches
-        for table, values in self.batches.items():
-            # Skip empty batches
-            if len(values) == 0:
-                continue
-
-            body = {
-                "majorDimension": "ROWS",
-                "values": values
-            }
-            result = self.spreadsheets.values().append(spreadsheetId=cSpreadSheetID, range=table, valueInputOption='USER_ENTERED', body=body).execute()
-            print('{0} cells updated for table {1}.'.format(result.get('updates').get('updatedCells'), table))
-
-            self.batches[table] = []
-        
-        # Reset last send time
-        self.last_batch_send = time.time()
-
-    def OpenProductivityWindow(self):
-        print("Spawning window")
-        # Create layout for window
-        layout = [ [sg.Text('How productive do you feel?')], 
-                    [ sg.Button(f'{x}') for x in range(1,11)]]
-
-        # Show window and read output
-        #TODO Wait nonblocking for the result
-        window = sg.Window('Productivity Tracker').Layout(layout)
-        event, values = window.Read()
-        window.Close()
-
-        # Write timestamp and productivity to sheet
-        values = [ str(datetime.datetime.now()), str(event)]
-        self.QueueRow(cProductivityTable, values)
-
-        # Update last pop up time
-        self.last_popup = time.time()
-
-    def ReportDistraction(self):
-        # Write timestamp and productivity to sheet
-        values = [ str(datetime.datetime.now())]
-        self.QueueRow(cDistractionsTable, values)
-
-        self.icon.show_notification('Reported distraction!')
- 
-
-    def main(self):
+        # Set up tray icon
         def icon_stop(inSysTrayIcon):
             self.stop()
         def icon_open(inSysTrayIcon):
@@ -196,61 +141,209 @@ class AppServerSvc (win32serviceutil.ServiceFramework):
         menu_options = (('Open', None, icon_open),
                         ('Report Productivity', None, icon_report_productivity),
                         ('Report Distraction', None, icon_report_distraction))
-        self.icon = SysTrayIcon.SysTrayIcon('ProductivityTracker.ico', 'Productivity Tracker', menu_options, on_quit=icon_stop, blocking=False)
+        self.mTrayIcon = SysTrayIcon.SysTrayIcon('ProductivityTracker.ico', 'Productivity Tracker', menu_options, on_quit=icon_stop, blocking=False)
 
-        self.icon.show_notification("Started!")
+        # Indicate that we should start running
+        self.mIsRunning = True
+
+
+    def stop(self):
+        self.mIsRunning = False
+        self.mMouseListener.stop()
+
+
+    def QueueRow(self, inTable, inRowValues):
+        # Create new batch array if it does not exist yet
+        if inTable not in self.mBatches:
+            self.mBatches[inTable] = []
+
+        # Append this row to the batch
+        self.mBatches[inTable].append(inRowValues)
+
+
+    def UpdateBatches(self):
+        # Wait for the interval to elapse
+        if time.time() - self.mLastBatchSend< cSendInterval:
+            return
+        
+        # Send every batch in the dict of batches
+        for table, values in self.mBatches.items():
+            # Skip empty batches
+            if len(values) == 0:
+                continue
+
+            body = {
+                "majorDimension": "ROWS",
+                "values": values
+            }
+            result = self.mSpreadsheetsService.values().append(spreadsheetId=cSpreadSheetID, range=table, valueInputOption='USER_ENTERED', body=body).execute()
+            print('{0} cells updated for table {1}.'.format(result.get('updates').get('updatedCells'), table))
+
+            self.mBatches[table] = []
+        
+        # Reset last send time
+        self.mLastBatchSend = time.time()
+
+
+    def OpenProductivityWindow(self):
+        print("Spawning window")
+        # Create layout for window
+        layout = [ [sg.Text('How productive do you feel?')], 
+                    [ sg.Button(f'{x}') for x in range(1,11)]]
+
+        # Show window and read output
+        #TODO Wait nonblocking for the result
+        self.mProductivityWindow = sg.Window('Productivity Tracker').Layout(layout)        
+
+        # Update last pop up time
+        self.mLastPopup = time.time()
+
+
+    def UpdateProductivityWindow(self):
+        if self.mProductivityWindow == None:
+            return
+        
+        form_event, form_values = self.mProductivityWindow.ReadNonBlocking()
+
+        # If event AND values are none, then the window was probably closed,
+        # so clean up and show again in a bit
+        if form_event == None and form_values == None:
+            self.mProductivityWindow.Close()
+            self.mProductivityWindow = None
+            # :TODO: Reschedule window open
+            return
+        
+        # Nothing happened, just wait until the user does something
+        if form_event == None:
+            return
+        
+        # By this point the user has pressed a button
+
+        # Close the form
+        self.mProductivityWindow.Close()
+        self.mProductivityWindow = None        
+
+        # Write timestamp and productivity to sheet
+        values = [ str(datetime.datetime.now()), str(form_event)]
+        self.QueueRow(cProductivityTable, values)
+
+
+    def UpdateProductivityTimer(self):
+        cNewDayThreshold    = 6 * 60*60
+        cAverageDelay       = 2 * 60*60
+        cRandomVariation    = 30*60
+
+
+        # If we've been locked for longer than 'a day', delete the current popup and defer any popup
+        # scheduling until next login
+        if self.mLockedSince != -1 and time.time() - self.mLockedSince > cNewDayThreshold:
+            if self.mProductivityWindow != None:
+                self.mProductivityWindow.Close()
+                self.mProductivityWindow = None
+            self.mScheduledPopup = -1
+            return
+            
+
+        # Check if there is a popup scheduled
+        if self.mScheduledPopup != -1:
+            # Show scheduled popup if it is time
+            if self.mScheduledPopup < time.time() and self.mScheduledPopup != -1:
+                self.OpenProductivityWindow()
+                self.mScheduledPopup = -1
+        else:
+            # Schedule a new popup
+            self.mScheduledPopup = time.time() + cAverageDelay + random.randint(-cRandomVariation, cRandomVariation)
+
+    def ReportDistraction(self):
+        # Write timestamp and productivity to sheet
+        values = [ str(datetime.datetime.now())]
+        self.QueueRow(cDistractionsTable, values)
+
+        self.mTrayIcon.show_notification('Reported distraction!')
+
+
+    def UpdateFocusedWindow(self):
+        try:
+            # Get the process for the currently focused window
+            whnd = win32gui.GetForegroundWindow()
+            window_text = win32gui.GetWindowText(whnd)
+            (_, pid) = win32process.GetWindowThreadProcessId(whnd)
+            handle = win32api.OpenProcess(win32con.PROCESS_ALL_ACCESS, False, pid)
+
+            # Get the executable name of this process
+            filename = win32process.GetModuleFileNameEx(handle, 0)
+            _, exe = os.path.split(filename)
+        except Exception:
+            # We might sometimes select something that does not have a process. Just ignore this event then
+            filename = None
+            exe = None
+
+        # Write timestamp, title text and exectuble name to sheet
+        values = [ str(datetime.datetime.now()), window_text, exe]
+        self.QueueRow(cFocusedProgramTable, values)
+
+        # If locked since last update, write the lock time. Reset lock time if any other program has focus
+        if exe == "LockApp.exe" and window_text == "Windows Default Lock Screen":
+            if self.mLockedSince == -1:
+                self.mLockedSince = time.time()
+                self.OnLock()
+        else:
+            # exe is None in the password screen, and due to an alt-tab bug.
+            # Having an actual program name definitely means we're logged in 
+            if exe != None and self.mLockedSince > 0:
+                self.mLockedSince = -1
+                self.OnUnlock()
+
+
+    def UpdateInput(self):
+        # Write timestamp, mouse and keyboard input to sheet
+        elapsed_time = time.time() - self.mLastUpdate
+        values = [    str(datetime.datetime.now()), 
+                        self.mMouseMoves / elapsed_time, 
+                        self.mMouseClicks / elapsed_time, 
+                        self.mMouseScrolls / elapsed_time, 
+                        self.mKeyboardPresses / elapsed_time]
+        self.QueueRow(cInputTable, values)
+
+        # Reset stored valeus
+        # Note: We might miss some inputs here because of the timeframe between writting and resetting. 
+        #       Theoretically we might also get incorrect values because of thread interrupts. This shouldn't be
+        #       common enough to be an issue though. 
+        self.mMouseMoves = 0
+        self.mMouseClicks = 0
+        self.mMouseScrolls = 0
+        self.mKeyboardPresses = 0
+
+
+    def main(self):
+        # Show a notification to confirm the main loop has started
+        # This is mostly here for debugging purposes
+        self.mTrayIcon.show_notification("Started!")
         
         # Main loop
-        while self.isrunning:
+        while self.mIsRunning:
             # Manually pump messages for the tray icon
             win32gui.PumpWaitingMessages()
+
+            # Update the window every frame, so it remains responsive
+            self.UpdateProductivityWindow()
             
-            # Sleep for a bit, running at roughly 30hz
-            time.sleep(0.033)
+            # Sleep for a bit, running at roughly 60hz
+            time.sleep(0.016)
             
             # Update at a lower interval than the message pump
-            if time.time() - self.last_update > cUpdateInterval:
-                try:
-                    # Get the process for the currently focused window
-                    whnd = win32gui.GetForegroundWindow()
-                    window_text = win32gui.GetWindowText(whnd)
-                    (_, pid) = win32process.GetWindowThreadProcessId(whnd)
-                    handle = win32api.OpenProcess(win32con.PROCESS_ALL_ACCESS, False, pid)
+            if time.time() - self.mLastUpdate > cUpdateInterval:
 
-                    # Get the executable name of this process
-                    filename = win32process.GetModuleFileNameEx(handle, 0)
-                    _, exe = os.path.split(filename)
-                except Exception:
-                    # We might sometimes select something that does not have a process. Just ignore this event then
-                    filename = None
-                    exe = None
-
-                # Write timestamp, title text and exectuble name to sheet
-                values = [ str(datetime.datetime.now()), window_text, exe]
-                self.QueueRow(cFocusedProgramTable, values)
-
-
-                # Write timestamp, mouse and keyboard input to sheet
-                elapsed_time = time.time() - self.last_update
-                values = [    str(datetime.datetime.now()), 
-                                self.mouse_moves / elapsed_time, 
-                                self.mouse_clicks / elapsed_time, 
-                                self.mouse_scrolls / elapsed_time, 
-                                self.keyboard_presses / elapsed_time]
-                self.QueueRow(cInputTable, values)
-                self.mouse_moves = 0
-                self.mouse_clicks = 0
-                self.mouse_scrolls = 0
-                self.keyboard_presses = 0
-
-                if time.time() - self.last_popup > 10000:
-                    self.OpenProductivityWindow()
+                self.UpdateInput()
+                self.UpdateFocusedWindow()
+                self.UpdateProductivityTimer()
 
                 # Send any batches if needed
                 self.UpdateBatches()
 
                 # Update timestamp
-                self.last_update = time.time()
+                self.mLastUpdate = time.time()
+
 
 if __name__ == '__main__':
     win32serviceutil.HandleCommandLine(AppServerSvc)
